fcc1f5dc368968b19f6aa00c83ed28379ec4e03d
Clément Walter <clement0walter@gmail.com>
2023-02-04 12:45:58 +0200

    Update deploy script with logger

--- packages/kakaswap-contracts/script/utils.py
+++ packages/kakaswap-contracts/script/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 import re
 import shlex
@@ -8,15 +9,19 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
+logger = logging.getLogger()
 
 deployments = {}
+RPC = os.environ["GOERLI_RPC_URL"]
 
 
 def deploy_contract(path: str, *args):
+    contract_name = path.split(":")[-1].replace(".sol", "")
+    logger.info(f"⏳ Deploying {contract_name}")
     res = subprocess.run(
         shlex.split(
             f"forge create "
-            + f"--rpc-url {os.environ['CONSENSYS_ZK_GOERLI_RPC_URL']} "
+            + f"--rpc-url {RPC} "
             + f"--private-key {os.environ['PRIVATE_KEY']} "
             + f"{path} "
             + (f"--constructor-args {' '.join(args)}" if args else ""),
@@ -30,17 +35,16 @@ def deploy_contract(path: str, *args):
     )
 
     if address is None:
-        raise ValueError(f"Cannot deploy {path}")
+        raise ValueError(f"Cannot deploy {path}: {res.stderr}")
 
     deployments[path.split(":")[-1].replace(".sol", "")] = address[1].strip()
+    logger.info(f"✅ {contract_name} deployed at {address[1].strip()}")
     return address[1].strip()
 
 
 chain_id = int(
     subprocess.run(
-        shlex.split(
-            f"cast chain-id --rpc-url {os.environ['CONSENSYS_ZK_GOERLI_RPC_URL']}"
-        ),
+        shlex.split(f"cast chain-id --rpc-url {RPC}"),
         capture_output=True,
     ).stdout[:-1]
 )
@@ -51,15 +55,16 @@ def dump():
     if Path("deployments.json").is_file():
         previous_deployments = json.loads(Path("deployments.json").read_text())
     _deployments = {
-        **previous_deployments["deployments"],
+        **previous_deployments.get("deployments", {}),
         **deployments["deployments"],
     }
     json.dump(
         {
-            "rpc": os.environ["CONSENSYS_ZK_GOERLI_RPC_URL"],
+            "rpc": RPC,
             "chain_id": chain_id,
             "deployments": _deployments,
         },
         open("deployments.json", "w"),
         indent=2,
     )
+    logger.info(f"✅ Deployments saved at deployments.json")
